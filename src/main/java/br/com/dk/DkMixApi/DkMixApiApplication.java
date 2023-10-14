dbd14af0f60eae2d0febc4614c7864579ab4fe6a
alankaiko <alamdfs@hotmail.com>
2023-10-14 11:08:58 -0300

    arquivos padrao

---
+++ src/main/java/br/com/dk/DkMixApi/DkMixApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dk.DkMixApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkMixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkMixApiApplication.class, args);
+	}
+
+}
